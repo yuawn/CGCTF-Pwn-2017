73c911c4785843ea9761d49e2ead12dbffa9e89a
yuan <ssspeed00@gmail.com>
2017-07-22 13:57:51 +0800

    CGCTF 2017

--- bof2/comp.sh
+++ bof2/comp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -m32 -mpreferred-stack-boundary=2 -fno-stack-protector bof.c -o /home/yuawn/challenges/pwn/bof2/home/bof2
+gcc -m32 -mpreferred-stack-boundary=2 -fno-stack-protector bof2.c -o /home/yuawn/challenges/pwn/bof2/home/bof2
